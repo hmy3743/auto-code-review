fd7719a0ed3f7d315cd5a067b3a6f2d65c63a72c
myHan <hmy3743@gmail.com>
2020-03-07 17:31:58 +0900

    initial commit

---
+++ src/test/java/com/myhan/autocodereview/AutoCodeReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myhan.autocodereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoCodeReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
